13d4b0c04165ab9cbc14f441b6c925f4b363d00b
Natalia Venditto <nataliafrontend@gmail.com>
2022-09-28 12:46:37 +0200

    feat: update data mock and typing

--- packages/portal/src/app/card-list/card-list.component.ts
+++ packages/portal/src/app/card-list/card-list.component.ts
@@ -9,47 +9,111 @@ import { CardType } from '../../typings/cardtype';
 export class CardListComponent implements OnInit {
   data  =  [
     {
-      "title": "Practical loft downtown",
-      "city": "Cologne",
-      "bedrooms": 2,
-      "bathrooms": 1,
-      "amenities": ["parking", "jacuzzi", "wi-fi"],
-      "img": "pic-bluegreen.png",
+      "title": "Bright apartment close to the park",
+      "city": "Edinburgh",
+      "bedrooms": 3,
+      "bathrooms": 3,
+      "amenities": ["no furniture", "fireplace", "garden"],
+      "description": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+      "img": "pic-salmon.png",
       "slug": {
-        "$numberLong": "488008"
+        "$numberLong": "366801"
       }
     },
     {
-      "title": "Bright apartment close to the park",
-      "city": "London",
-      "bedrooms": 2,
-      "bathrooms": 3,
-      "amenities": ["laundry room", "wi-fi", "furniture"],
-      "img": "pic-pink.png",
+      "title": "Luxurious house in the suburbs",
+      "city": "Guangzhou",
+      "bedrooms": 5,
+      "bathrooms": 1,
+      "amenities": ["laundry room", "furniture", "gym"],
+      "description": "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      "img": "pic-salmon.png",
       "slug": {
-        "$numberLong": "160979"
+        "$numberLong": "550298"
+      }
+    },
+    {
+      "title": "Luxurious house in the suburbs",
+      "city": "Milan",
+      "bedrooms": 3,
+      "bathrooms": 1,
+      "amenities": ["balcony", "sauna", "fireplace"],
+      "description": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+      "img": "pic-green.png",
+      "slug": {
+        "$numberLong": "307462"
+      }
+    },
+    {
+      "title": "Art-deco loft close to the metro station",
+      "city": "Tel Aviv",
+      "bedrooms": 1,
+      "bathrooms": 2,
+      "amenities": ["furniture", "patio", "terrace"],
+      "description": "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.",
+      "img": "pic-orange.png",
+      "slug": {
+        "$numberLong": "657862"
+      }
+    },
+    {
+      "title": "Beautiful house in the countryside",
+      "city": "New York",
+      "bedrooms": 5,
+      "bathrooms": 1,
+      "amenities": ["wi-fi", "jacuzzi", "terrace"],
+      "description": "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.",
+      "img": "pic-purple.png",
+      "slug": {
+        "$numberLong": "328668"
+      }
+    },
+    {
+      "title": "Luxurious house in the suburbs",
+      "city": "Paris",
+      "bedrooms": 4,
+      "bathrooms": 2,
+      "amenities": ["no furniture", "furniture", "balcony"],
+      "description": "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.",
+      "img": "pic-bluegreen.png",
+      "slug": {
+        "$numberLong": "803384"
       }
     },
     {
       "title": "Spacious apartment for a young family",
-      "city": "Charlotte",
-      "bedrooms": 1,
-      "bathrooms": 3,
-      "amenities": ["wi-fi", "dishwasher", "laundry room"],
-      "img": "pic-pink.png",
+      "city": "Johannesburg",
+      "bedrooms": 2,
+      "bathrooms": 1,
+      "amenities": ["parking", "microwave", "air conditioning"],
+      "description": "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.",
+      "img": "pic-orange.png",
       "slug": {
-        "$numberLong": "190731"
+        "$numberLong": "430000"
       }
     },
     {
-      "title": "Spacious house in the suburbs",
-      "city": "Tokyo",
-      "bedrooms": 4,
+      "title": "Modern apartment close to the metro station",
+      "city": "Beijing",
+      "bedrooms": 2,
       "bathrooms": 3,
-      "amenities": ["balcony", "no furniture", "heating"],
-      "img": "pic-purple.png",
+      "amenities": ["gym", "air conditioning", "swimming pool"],
+      "description": "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.",
+      "img": "pic-yellowgreen.png",
       "slug": {
-        "$numberLong": "434170"
+        "$numberLong": "121653"
+      }
+    },
+    {
+      "title": "Charming apartment close to the train station",
+      "city": "Beijing",
+      "bedrooms": 4,
+      "bathrooms": 2,
+      "amenities": ["laundry room", "microwave", "fireplace"],
+      "description": "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.",
+      "img": "pic-blue.png",
+      "slug": {
+        "$numberLong": "351150"
       }
     }
   ];
