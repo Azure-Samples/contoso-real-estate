acf11ba190b62dc9075f6da7d61845b8dc0d8474
Natalia Venditto <nataliafrontend@gmail.com>
2022-10-03 11:52:42 +0200

    feat: fix stage styling and imports

--- packages/portal/src/app/app.component.spec.ts
+++ packages/portal/src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'contoso-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('contoso-app');
   });
 
   it('should render title', () => {
