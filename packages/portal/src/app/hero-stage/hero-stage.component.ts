3c4324ab39321fd2a6b2c4345deeeab0f68f1f37
Natalia Venditto <nataliafrontend@gmail.com>
2022-10-03 11:52:42 +0200

    feat: fix stage styling and imports

--- packages/portal/src/app/hero-stage/hero-stage.component.ts
+++ packages/portal/src/app/hero-stage/hero-stage.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { StageType } from 'src/typings/stagetype';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-hero-stage',
   templateUrl: './hero-stage.component.html',
   styleUrls: ['./hero-stage.component.scss'],
   standalone: true,
-  imports: [ MatCardModule ]
+  imports: [ MatCardModule, MatButtonModule ]
 })
 export class HeroStageComponent implements OnInit {
   @Input()
