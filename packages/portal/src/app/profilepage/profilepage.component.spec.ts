f24667d8942dc93fa44e9fc467cb5cb0bd0b9390
glaucia86 <glaucia_lemos86@hotmail.com>
2022-12-07 16:22:07 -0300

    feat: generate via angular-cli 'profilepage'

---
+++ packages/portal/src/app/profilepage/profilepage.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfilepageComponent } from './profilepage.component';
+
+describe('ProfilepageComponent', () => {
+  let component: ProfilepageComponent;
+  let fixture: ComponentFixture<ProfilepageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProfilepageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProfilepageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ packages/portal/src/app/profilepage/profilepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profilepage',
+  templateUrl: './profilepage.component.html',
+  styleUrls: ['./profilepage.component.scss']
+})
+export class ProfilepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
