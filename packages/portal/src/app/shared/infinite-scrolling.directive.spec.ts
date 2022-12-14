fa3948cff76c73e5fa585f1cbc3d685f14295a06
Wassim Chegham <github@wassim.dev>
2022-12-14 18:03:52 +0100

    fix: make test pass

--- packages/portal/src/app/shared/infinite-scrolling.directive.spec.ts
+++ packages/portal/src/app/shared/infinite-scrolling.directive.spec.ts
@@ -1,8 +1,24 @@
-import { InfiniteScrollingDirective } from './infinite-scrolling.directive';
+import { Component, NgZone } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { InfiniteScrollingDirective } from "./infinite-scrolling.directive";
 
-describe('InfiniteScrollingDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InfiniteScrollingDirective(null, null);
+@Component({
+  standalone: true,
+  template: `<i appInfiniteScrolling></i>`,
+})
+class TestComponent {}
+
+describe("InfiniteScrollingDirective", () => {
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    fixture.autoDetectChanges();
+  });
+
+  it("should create an instance", () => {
+    const directive = new InfiniteScrollingDirective(fixture, new NgZone({}));
     expect(directive).toBeTruthy();
   });
 });
