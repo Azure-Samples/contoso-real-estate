1c332460f6592d7255fb945a654a40f0ca4017ef
Natalia Venditto <nataliafrontend@gmail.com>
2022-12-15 19:46:12 +0100

    chore: fix profile styles

--- packages/portal/src/app/profilepage/profilepage.component.ts
+++ packages/portal/src/app/profilepage/profilepage.component.ts
@@ -3,14 +3,18 @@ import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+// you were not importing the button module
+import { MatButtonModule } from "@angular/material/button";
 import { UserService } from '../shared/services/user-service/user.service';
+// I am importing the tabs here
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-profilepage',
   templateUrl: './profilepage.component.html',
   styleUrls: ['./profilepage.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatListModule, MatIconModule]
+  imports: [CommonModule, MatCardModule, MatListModule, MatIconModule, MatButtonModule, MatTabsModule]
 })
 export class ProfilepageComponent implements OnInit {
   title = "Profile User Page"
@@ -19,6 +23,8 @@ export class ProfilepageComponent implements OnInit {
     avatar: "",
 
   }
+  // This is just for the example. You can delete.
+  longText = `Doloribus magni saepe repellendus non corrupti soluta. Sint ipsa qui laboriosam enim aliquid ullam. Natus dolores aut enim maxime voluptatem incidunt alias voluptatum. Consequatur molestiae dolores eos ut. Dolore suscipit nostrum sint fugiat ipsa provident aut. Dolore minima optio.`;
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
