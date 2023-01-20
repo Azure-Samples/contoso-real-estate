8d3b7d0e2618da095a25ebb376b0802ef4e460de
Wassim Chegham <github@wassim.dev>
2023-01-20 16:43:34 +0100

    feat: add authorization directive

---
+++ packages/portal/src/app/shared/has-role/has-role.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HasRoleDirective } from './has-role.directive';
+
+describe('HasRoleDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HasRoleDirective();
+    expect(directive).toBeTruthy();
+  });
+});
