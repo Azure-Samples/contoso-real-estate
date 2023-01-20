7897355ea91c4d220ff4898a914ce2e26dabed9c
Wassim Chegham <github@wassim.dev>
2023-01-20 17:09:04 +0100

    chore: support login with multiple providers

--- packages/portal/src/app/authentication/authentication.component.ts
+++ packages/portal/src/app/authentication/authentication.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { ActivatedRoute } from '@angular/router';
-import { WindowService } from '../core/window/window.service';
-import { TextBlockComponent } from '../shared/text-block/text-block.component';
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AuthService } from "../shared/authentication/auth.service";
+import { TextBlockComponent } from "../shared/text-block/text-block.component";
 
 @Component({
-  selector: 'app-authentication',
-  templateUrl: './authentication.component.html',
-  styleUrls: ['./authentication.component.scss'],
-  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, TextBlockComponent],
+  selector: "app-authentication",
+  templateUrl: "./authentication.component.html",
+  styleUrls: ["./authentication.component.scss"],
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, TextBlockComponent],
   standalone: true,
 })
 export class AuthenticationComponent implements OnInit {
+  redirectURL = "/home";
 
-  redirectURL = "/";
+  providers = [
+    { name: "GitHub", id: "github" },
+    { name: "Google", id: "google" },
+    { name: "Microsoft", id: "microsoft" },
+    { name: "Twitter", id: "twitter" },
+    { name: "Facebook", id: "facebook" },
+    { name: "LinkedIn", id: "linkedin" },
+  ];
 
-  constructor(private route: ActivatedRoute, private windowService: WindowService) {}
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) {}
 
   ngOnInit(): void {
     const params = this.route.snapshot.queryParams;
@@ -26,34 +35,16 @@ export class AuthenticationComponent implements OnInit {
       this.redirectURL = params["redirectURL"];
     }
 
+    if (this.isAuthenticated()) {
+      this.router.navigate([this.redirectURL]);
+    }
+  }
+
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
   }
 
   loginWith(provider: string) {
-    this.windowService.nativeWindow().location.href = `/.auth/login/${provider}?post_login_redirect_uri=` + this.redirectURL;
+    return `/.auth/login/${provider}?post_login_redirect_uri=` + this.redirectURL;
   }
-
-  loginWithGithub() {
-    this.loginWith("github");
-  }
-
-  loginWithGoogle() {
-    this.loginWith("google");
-  }
-
-  loginWithMicrosoft() {
-    this.loginWith("aad");
-  }
-
-  loginWithTwitter() {
-    this.loginWith("twitter");
-  }
-
-  loginWithFacebook() {
-    this.loginWith("facebook");
-  }
-
-  loginWithLinkedIn() {
-    this.loginWith("linkedin");
-  }
-
 }
