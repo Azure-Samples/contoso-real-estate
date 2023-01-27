64c960d6dc0b2f70533c89822f0c15df071281bb
Natalia Venditto <nataliafrontend@gmail.com>
2023-01-27 15:59:57 +0000

    feat: fix nav styles

--- packages/blog/components/nav.tsx
+++ packages/blog/components/nav.tsx
@@ -4,30 +4,24 @@ import Link from "next/link"
 const Nav = ({ categories }) => {
   return (
     <div>
-      <nav className="uk-navbar-container" data-uk-navbar>
-        <div className="uk-navbar-left">
-          <ul className="uk-navbar-nav">
-            <li>
-              <Link href="/">Strapi Blog</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="uk-navbar-right">
-          <ul className="uk-navbar-nav">
-            {categories.map((category) => {
-              return (
-                <li key={category.id}>
-                  <Link
-                    href={`/category/${category.attributes.slug}`}
-                    className="uk-link-reset"
-                  >
-                    {category.attributes.name}
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
+      <nav className="nav">
+        <p className="text">
+          Welcome to our Blog! Choose your favorite category.
+        </p>
+        <ul className="nav-list">
+          {categories.map((category) => {
+            return (
+              <li key={category.id}>
+                <Link
+                  href={`/category/${category.attributes.slug}`}
+                  className="category label"
+                >
+                  {category.attributes.name}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
       </nav>
     </div>
   )
