a8d8d9a750ee2ed7d66b031ba5622851380625bb
Wassim Chegham <github@wassim.dev>
2023-01-28 00:09:35 +0100

    chore(portal): fix unit tests

--- packages/portal/src/app/shared/has-role/has-role.directive.spec.ts
+++ packages/portal/src/app/shared/has-role/has-role.directive.spec.ts
@@ -1,8 +1,26 @@
-import { HasRoleDirective } from './has-role.directive';
+import { Component, ViewChild } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HasRoleDirective } from "./has-role.directive";
 
-describe('HasRoleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HasRoleDirective();
-    expect(directive).toBeTruthy();
+@Component({
+  standalone: true,
+  template: `<span *appHasRole></span>`,
+  imports: [HasRoleDirective],
+})
+class TestComponent {
+  @ViewChild(HasRoleDirective, { static: true }) hasRole!: HasRoleDirective;
+}
+
+describe("HasRoleDirective", () => {
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    fixture.autoDetectChanges();
+  });
+
+  it("should create an instance", () => {
+    const { hasRole } = fixture.componentInstance;
+    expect(hasRole).toBeDefined();
   });
 });
