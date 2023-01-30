32cd8ef9736d37e94f862edc4c00207ceb05204a
Wassim Chegham <github@wassim.dev>
2023-01-30 11:53:34 +0100

    feat(cicd): setup CD

--- swa-cli-deploy.sh
+++ swa-cli-deploy.sh
@@ -6,18 +6,39 @@
 # Load environment variables from .env file
 export $(grep -v '^#' .env | xargs)
 
-# create a temporary folder
+# exit if SWA_CLI_DEPLOYMENT_TOKEN is not set
+if [ -z "${SWA_CLI_DEPLOYMENT_TOKEN}" ]; then
+    echo "SWA_CLI_DEPLOYMENT_TOKEN is not set. Please set it in your .env file or as a secret in your repository."
+    exit 1
+fi
+
+# set deploy_env to preview flag by default
+deploy_env="preview"
+git_branch=$(git rev-parse --abbrev-ref HEAD)
 tmpdir=$(mktemp -d)
 
+# set deploy_env to production flag if current branch is main
+if [ $git_branch = "main" ]; then
+  deploy_env="production"
+fi
+
+echo "SWA_CLI_DEPLOYMENT_TOKEN found!"
+echo "Current branch: $git_branch"
+echo "Deployment environment: $deploy_env"
+echo "Temporary folder: $tmpdir"
+
 # copy the API into the temporary folder
+echo "Copying API into temporary folder..."
 cp -r packages/api $tmpdir
 
 # build the API
+echo "Building API..."
 cd $tmpdir/api
 npm install
 npm run build
 npm prune --production
 
 # deploy the API using SWA CLI (load and override local swa config)
+echo "Deploying API..."
 cd -
-swa deploy --api-location=$tmpdir/api --deployment-token=$SWA_DEPLOYMENT_TOKEN
+swa deploy --api-location=$tmpdir/api --deployment-token=$SWA_CLI_DEPLOYMENT_TOKEN --env=$deploy_env --verbose=silly
