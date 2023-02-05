2a6a6d3847127fab39e489ba2361c3b14de2b070
Wassim Chegham <github@wassim.dev>
2023-02-05 19:03:48 +0100

    chore(portal): sync profile tabs with routes

--- packages/portal/src/app/profile/profile.component.ts
+++ packages/portal/src/app/profile/profile.component.ts
@@ -1,26 +1,63 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
-import { UserService } from '../shared/user/user.service';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatTabsModule } from "@angular/material/tabs";
+import { ActivatedRoute, RouterModule } from "@angular/router";
+import { FavoriteService } from "../shared/favorite.service";
+import { UserService } from "../shared/user/user.service";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"],
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatListModule, MatIconModule, MatButtonModule, MatTabsModule]
+  imports: [CommonModule, RouterModule, MatCardModule, MatListModule, MatIconModule, MatButtonModule, MatTabsModule],
 })
 export class ProfileComponent implements OnInit {
   user: User = {} as User;
-  constructor(private userService: UserService) { }
+  listings: Listing[] = [];
+  selectedTabIndex = 0;
 
-  async ngOnInit() {
-    this.user = await this.userService.currentUser();
-    console.log("User: ", this.user);
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private favoriteService: FavoriteService,
+  ) {
+    this.userService.user$.subscribe(user => {
+      this.user = user;
+    });
   }
 
+  async ngOnInit() {
+    this.route.data.subscribe(async data => {
+      this.user = data["user"];
+      await this.listFavorites();
+    });
+
+    const tabs = ["favorites", "payments", "reservations"];
+
+    this.route.paramMap.subscribe(async params => {
+      const tab = params.get("tab");
+      this.selectedTabIndex = tabs.indexOf(tab || "favorites");
+    });
+  }
+
+  async listFavorites() {
+    // this call allows us to get the count of favorites
+    this.listings = await this.favoriteService.countFavoritesByUser(this.user);
+
+    // this call allows us to get the actual listings
+    this.listings = await this.favoriteService.getFavoritesByUser(this.user);
+  }
+
+  async removeFavorite(listing: Listing) {
+    // remove the listing from the UI
+    this.listings = this.listings.filter(l => l.id !== listing.id);
+
+    // remove the favorite from the database
+    await this.favoriteService.removeFavorite(listing, this.user);
+  }
 }
