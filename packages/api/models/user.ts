1cb3982f37b81eb57020539ef903e4f3e7cdb052
Wassim Chegham <github@wassim.dev>
2023-02-05 19:16:07 +0100

    chore(api): improve storing User session

--- packages/api/models/user.ts
+++ packages/api/models/user.ts
@@ -1,16 +1,24 @@
 import UserModel, { User } from "../models/user.schema";
 
 export async function saveUserSession(user: User): Promise<User> {
-  const userModel = new UserModel(user);
-  await userModel.updateOne({ upsert: true });
-  return userModel;
-}
+  const { id: userId } = user;
 
+  if (userId) {
+    const existingModel = await UserModel.findOne({ id: userId });
+    if (existingModel) {
+      // Find by user ID not mongo ObjectId
+      await UserModel.updateOne({ id: userId }, user);
+      return existingModel;
+    }
+  }
+
+  return await UserModel.create(user);
+}
 
 export async function findUserById(id: string): Promise<User | null> {
   return await UserModel.findById(id);
 }
 
-export async function findUsers({offset, limit}: {offset: number, limit: number}): Promise<User[]> {
+export async function findUsers({ offset, limit }: { offset: number; limit: number }): Promise<User[]> {
   return await UserModel.find().skip(offset).limit(limit);
 }
