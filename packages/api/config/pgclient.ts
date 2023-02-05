3eb64e223bf5e68c3644e639f5e0e0ac780559f3
Wassim Chegham <github@wassim.dev>
2023-02-05 19:12:03 +0100

    chore(api): add logs to pgquery
    Also, make args optional

--- packages/api/config/pgclient.ts
+++ packages/api/config/pgclient.ts
@@ -2,17 +2,19 @@ import pg from "pg";
 import { logger } from "./observability";
 import { getConfig } from "./index";
 
-export const pgQuery = async (transaction: string, args: any[]) => {
+export async function pgQuery(transaction: string, args?: any[]) {
   let client;
   try {
     const config = (await getConfig()).strapi;
     client = new pg.Client(config);
 
+    console.log("Connecting to PostgreSQL database...");
     await client.connect();
+    console.log("Connected to PostgreSQL database");
 
     return await client.query(transaction, args);
   } catch (err) {
-    logger.error(`PostgreSQL client database error: ${err}`);
+    logger.error(`PostgreSQL database error: ${err}`);
     throw err;
   } finally {
     await client?.end();
