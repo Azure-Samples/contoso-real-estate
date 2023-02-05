7f5b66427d34c75f004b240ff95a2b0ba398dc01
Wassim Chegham <github@wassim.dev>
2023-02-05 21:38:37 +0100

    fix(portal): make tests work

--- packages/portal/src/app/rentalpage/rentalpage.component.spec.ts
+++ packages/portal/src/app/rentalpage/rentalpage.component.spec.ts
@@ -1,7 +1,29 @@
+import { CommonModule } from "@angular/common";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Router } from "@angular/router";
 import { RouterTestingModule } from "@angular/router/testing";
 import { RentalpageComponent } from "./rentalpage.component";
+
+const mockRouter = {
+  getCurrentNavigation: jest.fn().mockReturnValue({
+    extras: {
+      state: {
+        listing: {
+          id: "123",
+          name: "foo",
+          description: "bar",
+          photos: ["baz", "qux"],
+          address: ["quux", "quuz", "corge"],
+        },
+        user: {
+          id: "456",
+          name: "baz",
+        },
+      },
+    },
+  }),
+};
 
 describe("RentalpageComponent", () => {
   let component: RentalpageComponent;
@@ -10,9 +32,10 @@ describe("RentalpageComponent", () => {
   beforeEach(async () => {
     TestBed.overrideComponent(RentalpageComponent, {
       set: {
-        imports: [RouterTestingModule],
+        imports: [RouterTestingModule, CommonModule],
+        providers: [{ provide: Router, useValue: mockRouter }],
         schemas: [NO_ERRORS_SCHEMA],
-      }
+      },
     });
     fixture = TestBed.createComponent(RentalpageComponent);
     fixture.autoDetectChanges();
