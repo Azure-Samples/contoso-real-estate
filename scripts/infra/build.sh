f1abbea8b891298d4492527c9b39b3d6020a03f5
sinedied <noda@free.fr>
2023-02-13 14:42:18 +0100

    build: fix build on osx arm

--- scripts/infra/build.sh
+++ scripts/infra/build.sh
@@ -18,7 +18,7 @@ npm run build --workspace=portal
 npm run build --workspace=api
 
 # Build all docker images ----------------------------------------------------
-npm run docker:build --if-present --workspaces
+npm run docker:build --if-present --workspaces -- --platform linux/amd64
 
 # Tag and push cms docker image ----------------------------------------------
 docker image tag cms "$REGISTRY_NAME.azurecr.io/cms:v1"
@@ -31,3 +31,5 @@ docker image push "$REGISTRY_SERVER/blog:v1"
 # Tag and push stripe docker image -----------------------------------------
 # docker image tag cms "$REGISTRY_NAME.azurecr.io/stripe:v1"
 # docker image push "$REGISTRY_SERVER/stripe:v1"
+
+echo "Build complete."
