9c0122cdbda2f5dac96c0457ace245e7fdbbf58a
sinedied <noda@free.fr>
2023-02-17 09:58:59 +0100

    chore: update deploy script

--- scripts/infra/deploy.sh
+++ scripts/infra/deploy.sh
@@ -20,41 +20,6 @@ npx -y @azure/static-web-apps-cli@1.0.6 deploy \
 popd
 echo "Portal deployed to $SWA_PORTAL_URL"
 
-# Deploy api function --------------------------------------------------------
-echo "Deploying api..."
-
-# Copy API to temporary folder as NPM workspaces are not supported by
-# Azure Functions Core Tools CLI
-api_tmpdir=$(mktemp -d)
-cp -r packages/api "$api_tmpdir"
-pushd "$api_tmpdir/api"
-npm install --omit=dev
-npx -y azure-functions-core-tools@4 azure functionapp publish "$FUNCTION_API_NAME" \
-  --typescript \
-  --verbose
-rm -rf "$api_tmpdir"
-popd
-
-# Update settings
-az functionapp config appsettings set \
-  --name "$FUNCTION_API_NAME" \
-  --resource-group "$RESOURCE_GROUP_NAME" \
-  --settings \
-    "APP_DOMAIN=$SWA_PORTAL_URL" \
-    "APPINSIGHTS_INSTRUMENTATIONKEY=$APP_INSIGHTS_KEY" \
-    "APPLICATIONINSIGHTS_CONNECTION_STRING=$APP_INSIGHTS_CONNECTION_STRING" \
-    "DATABASE_CONNECTION_STRING=$MONGO_CONNECTION_STRING" \
-    "DATABASE_NAME=$MONGO_DATABASE_NAME" \
-    "STRIPE_PUBLIC_KEY=$STRIPE_PUBLIC_KEY" \
-    "STRIPE_SECRET_KEY=$STRIPE_SECRET_KEY" \
-    "STRIPE_WEBHOOK_SECRET=$STRIPE_WEBHOOK_SECRET" \
-    "STRAPI_DATABASE_HOST=$STRAPI_DATABASE_HOST" \
-    "STRAPI_DATABASE_NAME=$STRAPI_DATABASE_NAME" \
-    "STRAPI_DATABASE_USERNAME=$STRAPI_DATABASE_USERNAME" \
-    "STRAPI_DATABASE_PASSWORD=$STRAPI_DATABASE_PASSWORD" \
-    "STRAPI_DATABASE_PORT=$STRAPI_DATABASE_PORT" \
-    "STRAPI_DATABASE_SSL=$STRAPI_DATABASE_SSL"
-
 # Deploy cms container app ---------------------------------------------------
 echo "Deploying cms..."
 container_app_cms_host=$(
@@ -68,14 +33,14 @@ container_app_cms_host=$(
     --image "$REGISTRY_SERVER/cms:v1" \
     --target-port 1337 \
     --ingress external \
-    --secrets   "databaseusername=$STRAPI_DATABASE_USERNAME" \
-                "databasepassword=$STRAPI_DATABASE_PASSWORD" \
-    --env-var   "DATABASE_HOST=$STRAPI_DATABASE_HOST" \
-                "DATABASE_PORT=$STRAPI_DATABASE_PORT" \
-                "DATABASE_NAME=$STRAPI_DATABASE_NAME" \
-                "DATABASE_SSL=$STRAPI_DATABASE_SSL" \
-                "DATABASE_USERNAME=secretref:databaseusername" \
-                "DATABASE_PASSWORD=secretref:databasepassword" \
+    --secrets "databaseusername=$STRAPI_DATABASE_USERNAME" \
+              "databasepassword=$STRAPI_DATABASE_PASSWORD" \
+    --env-var "DATABASE_HOST=$STRAPI_DATABASE_HOST" \
+              "DATABASE_PORT=$STRAPI_DATABASE_PORT" \
+              "DATABASE_NAME=$STRAPI_DATABASE_NAME" \
+              "DATABASE_SSL=$STRAPI_DATABASE_SSL" \
+              "DATABASE_USERNAME=secretref:databaseusername" \
+              "DATABASE_PASSWORD=secretref:databasepassword" \
     --scale-rule-name http-rule \
     --scale-rule-type http \
     --scale-rule-http-concurrency 1000 \
@@ -114,7 +79,7 @@ echo "Blog deployed to $container_app_blog_url"
 echo "Deploying stripe..."
 container_app_stripe_host=$(
   az containerapp create \
-    --name "$CONTAINER_APP_BLOG_NAME" \
+    --name "$CONTAINER_APP_STRIPE_NAME" \
     --resource-group "$RESOURCE_GROUP_NAME" \
     --environment "$CONTAINER_APP_ENV_NAME" \
     --registry-server "$REGISTRY_SERVER" \
@@ -123,7 +88,11 @@ container_app_stripe_host=$(
     --image "$REGISTRY_SERVER/stripe:v1" \
     --target-port 3000 \
     --ingress external \
-    \ # --env-vars "NEXT_PUBLIC_STRAPI_API_URL=$container_app_cms_url" \
+    --env-vars "WEB_APP_URL=$SWA_PORTAL_URL" \
+               "API_URL=$FUNCTION_API_URL" \
+               "STRIPE_PUBLIC_KEY=$STRIPE_PUBLIC_KEY" \
+               "STRIPE_SECRET_KEY=$STRIPE_SECRET_KEY" \
+               "STRIPE_WEBHOOK_SECRET=$STRIPE_WEBHOOK_SECRET" \
     --scale-rule-name http-rule \
     --scale-rule-type http \
     --scale-rule-http-concurrency 1000 \
@@ -132,6 +101,38 @@ container_app_stripe_host=$(
     --output tsv
 )
 container_app_stripe_url="https://$container_app_stripe_host"
-echo "Blog deployed to $container_app_stripe_url"
+echo "Stripe deployed to $container_app_stripe_url"
+
+# Deploy api function --------------------------------------------------------
+echo "Deploying api..."
+
+# Copy API to temporary folder as NPM workspaces are not supported by
+# Azure Functions Core Tools CLI
+api_tmpdir=$(mktemp -d)
+cp -r packages/api "$api_tmpdir"
+pushd "$api_tmpdir/api"
+npm install --omit=dev
+npx -y azure-functions-core-tools@4 azure functionapp publish "$FUNCTION_API_NAME" \
+  --typescript \
+  --verbose
+rm -rf "$api_tmpdir"
+popd
+
+# Update settings
+az functionapp config appsettings set \
+  --name "$FUNCTION_API_NAME" \
+  --resource-group "$RESOURCE_GROUP_NAME" \
+  --settings \
+    "STRIPE_SERVICE_URL=$container_app_stripe_url" \
+    "APPINSIGHTS_INSTRUMENTATIONKEY=$APP_INSIGHTS_KEY" \
+    "APPLICATIONINSIGHTS_CONNECTION_STRING=$APP_INSIGHTS_CONNECTION_STRING" \
+    "MONGO_CONNECTION_STRING=$MONGO_CONNECTION_STRING" \
+    "MONGO_DATABASE_NAME=$MONGO_DATABASE_NAME" \
+    "STRAPI_DATABASE_HOST=$STRAPI_DATABASE_HOST" \
+    "STRAPI_DATABASE_NAME=$STRAPI_DATABASE_NAME" \
+    "STRAPI_DATABASE_USERNAME=$STRAPI_DATABASE_USERNAME" \
+    "STRAPI_DATABASE_PASSWORD=$STRAPI_DATABASE_PASSWORD" \
+    "STRAPI_DATABASE_PORT=$STRAPI_DATABASE_PORT" \
+    "STRAPI_DATABASE_SSL=$STRAPI_DATABASE_SSL"
 
 echo "Deployment complete."
