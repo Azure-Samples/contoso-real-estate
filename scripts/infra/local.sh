a69f8b0231a1d26c873954e430cf3440fb20c6ee
sinedied <noda@free.fr>
2023-02-17 09:58:27 +0100

    feat: add script to generate local env files

---
+++ scripts/infra/local.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+##############################################################################
+# Usage: ./local.sh
+# Creates a local development .env setup for the Contoso App.
+##############################################################################
+# v1.0.0 | dependencies: grep, xargs
+##############################################################################
+
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+source .env
+source .stripe.env
+cd ../..
+
+local_env_file=".env.local"
+docker_env_file=".env.docker"
+
+if [[ -f "$local_env_file" ]]; then
+  echo "Local .env file already exists. Skipping."
+  exit 0
+fi
+
+echo "Creating local .env file..."
+cat ./scripts/infra/.env > $local_env_file
+echo >> $local_env_file
+
+echo "# Stripe environment variables" >> $local_env_file
+echo >> $local_env_file
+cat ./scripts/infra/.stripe.env >> $local_env_file
+echo >> $local_env_file
+
+echo "# Function API environment variables" >> $local_env_file
+echo >> $local_env_file
+echo "APPINSIGHTS_INSTRUMENTATIONKEY='$APP_INSIGHTS_KEY'" >> $local_env_file
+echo "APPLICATIONINSIGHTS_CONNECTION_STRING='$APP_INSIGHTS_CONNECTION_STRING'" >> $local_env_file
+echo "STRIPE_SERVICE_URL='http://localhost:4242'" >> $local_env_file
+echo >> $local_env_file
+
+echo "# Stripe API environment variables" >> $local_env_file
+echo >> $local_env_file
+echo "WEB_APP_URL='$SWA_PORTAL_URL'" >> $local_env_file
+echo "API_URL='https://localhost:7071'" >> $local_env_file
+echo >> $local_env_file
+
+echo "# Blog environment variables" >> $local_env_file
+echo >> $local_env_file
+echo "NEXT_PUBLIC_STRAPI_API_URL='http://localhost:1337'" >> $local_env_file
+echo >> $local_env_file
+
+echo "# CMS environment variables" >> $local_env_file
+echo >> $local_env_file
+echo "DATABASE_HOST='$STRAPI_DATABASE_HOST'" >> $local_env_file
+echo "DATABASE_PORT='$STRAPI_DATABASE_PORT'" >> $local_env_file
+echo "DATABASE_NAME='$STRAPI_DATABASE_NAME'" >> $local_env_file
+echo "DATABASE_SSL='$STRAPI_DATABASE_SSL'" >> $local_env_file
+echo "DATABASE_USERNAME='$STRAPI_DATABASE_USERNAME'" >> $local_env_file
+echo "DATABASE_PASSWORD='$STRAPI_DATABASE_PASSWORD'" >> $local_env_file
+echo >> $local_env_file
+
+echo "Local .env file created."
+
+# Docker run doesn't support quotes when using --env-file flag, so we need to
+# create a docker-specific .env file without quotes.
+
+echo "Create a docker-specific .env file..."
+grep -v '^#' $local_env_file | xargs -n 1 > $docker_env_file
+echo "Docker .env file created."
