06293f3b8bbfc0ab8c58a59cb17f9abc21b481da
Natalia Venditto <nataliafrontend@gmail.com>
2023-04-15 10:25:35 +0000

    feat: add searchpage to portal

---
+++ packages/portal/src/app/searchpage/searchpage.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchpage',
+  templateUrl: './searchpage.component.html',
+  styleUrls: ['./searchpage.component.scss'],
+  standalone: true
+})
+export class SearchpageComponent implements OnInit {
+
+
+}
