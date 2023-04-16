335d730908f228ee31d799f8cff9870128942a08
Natalia Venditto <nataliafrontend@gmail.com>
2023-04-16 10:35:14 +0000

    feat: add graphql and apollo to search service

--- packages/portal/src/app/searchpage/searchpage.component.ts
+++ packages/portal/src/app/searchpage/searchpage.component.ts
@@ -1,14 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIcon } from '@angular/material/icon';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { SearchService } from '../shared/search/search.service';
 
 @Component({
   selector: 'app-searchpage',
   templateUrl: './searchpage.component.html',
   styleUrls: ['./searchpage.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule]
 })
 export class SearchpageComponent implements OnInit {
+  searchForm!: FormGroup;
+  searchTerm: FormControl = new FormControl();
+  results?: Observable<any>;
+  term = '';
+  isLoading = false;
 
-  async ngOnInit() {
+  constructor(
+    private searchService: SearchService,
+    private fb: FormBuilder
+  ) {}
+
+  async ngOnInit(): Promise<void> {
     console.log('searchpage');
+
+    this.searchForm = new FormGroup({
+      term: new FormControl('', Validators.minLength(2))
+    });
+  }
+
+  onSubmit(): void {
+    console.log('submit');
+    this.results = this.searchService.getResults(this.searchForm.value.term);
   }
 }
--- packages/portal/src/app/shared/search/search.service.ts
+++ packages/portal/src/app/shared/search/search.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
+
+export interface SearchResult {
+  result: Listing
+}
+
+const GetResults = gql`
+query getResults($term: String!) {
+  listings(filters: {or: [{title: {contains: $term}}, {description: {contains: $term}}]}) {
+    data {
+      attributes {
+        title
+        description
+      }
+    }
+  }
+}
+`;
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class SearchService {
 
-  constructor() { }
+  constructor(
+    @Inject(Apollo) private apollo: Apollo
+  ) { }
+
+  getResults = (term: string) => {
+    return this.apollo.watchQuery<SearchResult>({
+      query: GetResults,
+      variables: {
+        term: term
+      }
+    }).valueChanges;
+  }
 }
