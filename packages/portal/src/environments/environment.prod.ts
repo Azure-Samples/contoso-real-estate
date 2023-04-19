0681253e52f57c0b14dc3cbd2576853e78de71bd
Wassim Chegham <github@wassim.dev>
2023-04-19 09:02:16 +0000

    fix: use 127.0.0.1 insteadof localhost for graphql

--- packages/portal/src/environments/environment.prod.ts
+++ packages/portal/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  blogUrl: 'https://aka.ms/contoso-real-estate/blog-prod'
+  blogUrl: 'https://aka.ms/contoso-real-estate/blog-prod',
+
+  // Note: use 127.0.0.1 instead of localhost to avoid Codespaces issues
+  strapiGraphQlUri: 'http://127.0.0.1:1337/graphql'
 };
