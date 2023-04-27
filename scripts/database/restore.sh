7b0fdef96e3c4da66e04ed5656f39a18745e36fe
Wassim Chegham <github@wassim.dev>
2023-04-27 23:05:11 +0200

    chore: simplify restoring db

--- scripts/database/restore.sh
+++ scripts/database/restore.sh
@@ -12,12 +12,15 @@ azd env get-values > .env
 source .env
 rm .env
 
-file="${1:-}"
+filename="${1:-}"
+file="$(pwd)/dumps/$filename.sql"
 
 if [[ -z "$file" ]]; then
   echo "Usage: ./restore.sh <dump_file>"
   exit 1
 fi
+
+echo "Restoring PostgreSQL Database from $file"
 
 # Restore strapi database ----------------------------------------------------
 PGPASSWORD="$STRAPI_DATABASE_PASSWORD" pg_restore -v \
@@ -27,5 +30,10 @@ PGPASSWORD="$STRAPI_DATABASE_PASSWORD" pg_restore -v \
   --username="$STRAPI_DATABASE_USERNAME" \
   --dbname="$STRAPI_DATABASE_NAME" \
   "$file" || true
+
+if [[ $? -ne 0 ]]; then
+  echo "!!Failed to restore PostgreSQL Database"
+  exit 1
+fi
 
 echo "PostgreSQL Database restored successfully"
