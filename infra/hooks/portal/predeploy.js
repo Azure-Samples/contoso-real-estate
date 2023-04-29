8a092c51452ba0d58c29790a3033547a127f5c51
Wassim Chegham <github@wassim.dev>
2023-04-29 03:04:34 +0200

    chore: improve life cycle hook

--- infra/hooks/portal/predeploy.js
+++ infra/hooks/portal/predeploy.js
@@ -1,22 +1,45 @@
-const { execSync } = require("child_process");
-const { readFileSync, writeFileSync } = require("fs");
+#!/usr/bin/env node
 
-const filePath = "packages/portal/src/environments/environment.prod.ts";
-const oldValue = "https://aka.ms/contoso-real-estate/blog-prod";
+// This script is used to replace the blog URL in the portal environment
+
+const { execSync } = require("node:child_process");
+const { readFileSync, readdirSync, writeFileSync } = require("node:fs");
+const { resolve, join } = require("node:path");
+
 const shellCommand = `azd env get-values`;
+const envVars = execSync(shellCommand).toString().trim();
 const regex = /SERVICE_BLOG_URI="([^"]+)"/;
+const oldValue = "SERVICE_BLOG_URI_PLACEHOLDER";
+// Note: this script is run by azd from the root of ./packages/portal
+const distPath = resolve(__dirname, "../../../packages/portal/dist/contoso-app");
 
-const newValue = execSync(shellCommand).toString().trim();
-const match = newValue.match(regex);
+function replaceBlogUrl(filePath) {
+  const match = envVars.match(regex);
 
-if (match) {
-  const value = match[1];
-  const fileContents = readFileSync(filePath, "utf-8");
-  const newFileContents = fileContents.replace(oldValue, value);
-  writeFileSync(filePath, newFileContents);
-
-  process.exit(0);
-
-} else {
-  process.exit(1);
+  if (match) {
+    const value = match[1];
+    const fileContents = readFileSync(filePath, "utf-8");
+    const newFileContents = fileContents.replace(oldValue, value);
+    writeFileSync(filePath, newFileContents);
+    console.log(`Replaced ${oldValue} with ${value} in ${filePath}`);
+  }
+  else {
+    console.log(`No match found for ${regex}. Skipping replacement.`);
+  }
 }
+
+function findMainFile(directoryPath) {
+  try {
+    const files = readdirSync(directoryPath);
+    for (const file of files) {
+      if (file.startsWith("main.")) {
+        replaceBlogUrl(join(distPath, file));
+        break;
+      }
+    }
+  } catch (err) {
+    console.error("Error reading directory:", err);
+  }
+}
+
+findMainFile(distPath);
