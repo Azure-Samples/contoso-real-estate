0cff266355d4ba88345578cb1027d07fb3a9925e
Wassim Chegham <github@wassim.dev>
2023-05-02 10:55:34 +0200

    chore: add explainer for SERVICE_BLOG_URI_PLACEHOLDER

--- packages/portal/src/environments/environment.prod.ts
+++ packages/portal/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
+
+  // Note: this placeholder will be replaced by the actual URL during the build process.
+  // If you hard-code the URL here, it will be used instead of the placeholder and
+  // no replacement will be made.
   blogUrl: 'SERVICE_BLOG_URI_PLACEHOLDER',
 };
