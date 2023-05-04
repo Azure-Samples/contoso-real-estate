e8950a6f2c0ad763b4c2fdc995c74f8ba34d8a41
Wassim Chegham <github@wassim.dev>
2023-05-04 10:34:31 +0200

    ci: freeze node 18.15 on postinstall

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 sudo apt update && sudo apt-get install postgres
+nvm install 18.15.0 # See https://github.com/Azure-Samples/contoso-real-estate/pull/191
+
 npm i -g npm@latest fuzz-run
 npm install
 curl -fsSL https://aka.ms/install-azd.sh | bash
