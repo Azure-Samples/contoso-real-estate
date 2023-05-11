775916302fdd99a1062df8f4d027662725035793
Wassim Chegham <github@wassim.dev>
2023-05-11 06:27:45 +0200

    fix: resolve next remainig conflicts

--- packages/blog/components/stage.tsx
+++ packages/blog/components/stage.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link"
+import Image from "next/image"
 import { loadHomePage } from "../lib/services"
 import Nav from "./nav"
 
-const Stage = () => {
+const Stage = async () => {
   const homepage = await loadHomePage()
   const portalUrl = process.env.NEXT_PUBLIC_PORTAL_URL || "/"
   return (
     <div className="stage">
-      <img
+      <Image
         src="/images/contoso-real-estate-logo.svg"
         alt="Contoso Real Estate Fictional Company Logo"
         width="200"
