2ef972e1a9a96896ffc251f0ef28e33e78cf0335
Natalia Venditto <nataliafrontend@gmail.com>
2023-06-15 11:34:06 +0200

    chore: optimize guard

--- packages/portal/src/app/shared/guards/auth-guard.service.ts
+++ packages/portal/src/app/shared/guards/auth-guard.service.ts
@@ -2,13 +2,13 @@ import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "../authentication/auth.service";
 
-
 export const canActiveAuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  const isLoggedIn = inject(AuthService).isAuthenticated()
-  if(isLoggedIn) {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const isLoggedIn = authService.isAuthenticated();
+  if (isLoggedIn) {
     return true;
-  } else {
-    inject(Router).navigate(["/auth/login"], {queryParams: {redirectURL: state.url}});
-  }
+  } 
+  router.navigate(["/auth/login"], {queryParams: {redirectURL: state.url}});
   return false;
 }
