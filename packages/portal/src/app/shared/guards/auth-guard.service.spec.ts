8bdea988d45847fe71ce61e83de7c5aa98c57390
Lars Gyrup Brink Nielsen <layzee.dk@gmail.com>
2023-06-23 21:32:28 +0000

    test(portal): implement auth guard tests

--- packages/portal/src/app/shared/guards/auth-guard.service.spec.ts
+++ packages/portal/src/app/shared/guards/auth-guard.service.spec.ts
@@ -1,21 +1,95 @@
-import { TestBed } from '@angular/core/testing';
-import { AuthGuard } from './auth-guard.service';
+import { provideLocationMocks } from "@angular/common/testing";
+import { Component, EnvironmentProviders, makeEnvironmentProviders, ValueProvider } from "@angular/core";
+import { TestBed } from "@angular/core/testing";
+import { provideRouter } from "@angular/router";
+import { RouterTestingHarness } from "@angular/router/testing";
+import { of } from "rxjs";
+import { UserRole, UserService } from "../user/user.service";
+import { canActiveAuthGuard } from "./auth-guard.service";
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve({})
-  })
-) as any;
+@Component({
+  standalone: true,
+  template: "",
+})
+class DummyAuthenticationComponent {}
 
-describe('AuthGuard', () => {
-  let service: AuthGuard;
+@Component({
+  standalone: true,
+  template: "",
+})
+class DummyProfileComponent {}
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthGuard);
+function provideUserTesting(options: { userRole: UserRole }): EnvironmentProviders {
+  const guestUser: User = {
+    id: "guest",
+    name: "Guest",
+    photo: "account_circle",
+    role: UserRole.Guest,
+  };
+  const testUser: User = {
+    id: "be092a414817ff761949e046f4adf169",
+    name: "Test User",
+    photo: "account_circle",
+    role: options.userRole,
+    email: "testuser@example.com",
+    address: "fake address",
+    auth: {
+      provider: "github",
+      lastLogin: new Date().toISOString(),
+    },
+  };
+  const userServiceStub: Partial<UserService> = {
+    user$: of(options.userRole === UserRole.Guest ? guestUser : testUser),
+  };
+  const userServiceTestingProvider: ValueProvider = {
+    provide: UserService,
+    useValue: userServiceStub,
+  };
+
+  return makeEnvironmentProviders([userServiceTestingProvider]);
+}
+
+async function setup(options: { userRole: UserRole }) {
+  TestBed.configureTestingModule({
+    providers: [
+      provideUserTesting({ userRole: options.userRole }),
+      provideRouter([
+        {
+          path: "me",
+          canActivate: [canActiveAuthGuard],
+          component: DummyProfileComponent,
+        },
+        {
+          path: "auth/login",
+          component: DummyAuthenticationComponent,
+        },
+      ]),
+      provideLocationMocks(),
+    ],
+  });
+  const harness = await RouterTestingHarness.create();
+
+  return {
+    harness,
+  };
+}
+
+describe("canActiveAuthGuard", () => {
+  it("accepts renters", async () => {
+    const { harness } = await setup({ userRole: UserRole.Renter });
+
+    await harness.navigateByUrl("/me", DummyProfileComponent);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it("accepts admins", async () => {
+    const { harness } = await setup({ userRole: UserRole.Admin });
+
+    await harness.navigateByUrl("/me", DummyProfileComponent);
+  });
+
+  it("rejects guests", async () => {
+    const { harness } = await setup({ userRole: UserRole.Guest });
+
+    await harness.navigateByUrl("/me", DummyAuthenticationComponent);
   });
 });
