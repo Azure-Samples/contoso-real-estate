58f3bed43dc4d9fba1711a5895fc86e08ad96645
Lars Gyrup Brink Nielsen <layzee.dk@gmail.com>
2023-06-23 19:55:20 +0000

    refactor(portal): remove unused `enableProdMode`
    It is no longer required unless we specifically need the Angular JIT compiler.

--- packages/portal/src/main.ts
+++ packages/portal/src/main.ts
@@ -1,13 +1,7 @@
-import { enableProdMode } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
 import { UserService } from "./app/shared/user/user.service";
-import { environment } from "./environments/environment";
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, appConfig).then(async app => {
   const userService = app.injector.get(UserService);
