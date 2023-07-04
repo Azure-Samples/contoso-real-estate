8d57bead8c5905920570b8007c68d9f05c08a9d3
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-07-04 19:18:45 +0000

    feat(api-v4): initial commit with v4 programming model httpTriggerTest.ts

---
+++ packages/api-v4/src/functions/httpTriggerTest.ts
@@ -0,0 +1,20 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function httpTriggerTest(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  const name = request.query.get('name') || await request.text() || 'I will be right back!';
+
+  console.log("Console...", name);
+
+  return { body: `Hello, Developers! ${name}!` };
+
+};
+
+
+
+app.http('httpTriggerTest', {
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  handler: httpTriggerTest
+});
