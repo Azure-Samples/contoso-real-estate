e52d439e4c2a709954fadde188c21b51c1943a93
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-07-05 16:59:31 +0000

    fix(api-v4): remove unecessary comments

--- packages/api-v4/src/models/user.ts
+++ packages/api-v4/src/models/user.ts
@@ -31,7 +31,6 @@ export async function findUserById(id: string): Promise<User | null> {
 }
 
 export async function findUsers({ offset, limit }: { offset: number; limit: number }): Promise<User[]> {
-  //return await UserModel.find().skip(offset).limit(limit);
   return await UserModel.find().skip(offset).limit(limit);
 }
 
