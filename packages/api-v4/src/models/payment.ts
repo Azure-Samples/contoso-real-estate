feac0faf4b8bdbdf92f950aac10985e792e270db
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-07-17 21:51:55 +0000

    chore(api-v4): development of the 'payment.ts' file

---
+++ packages/api-v4/src/models/payment.ts
@@ -0,0 +1,37 @@
+/**
+ * file: packages/api-v4/src/models/payment.ts
+ * description: file responsible for the 'Payment' model
+ * data: 07/18/2023
+ * author: Glaucia Lemos
+ */
+
+import { PaymentModel, Payment } from "./payment.schema";
+
+export async function savePayment(payment: Partial<Payment>): Promise<Payment> {
+  return PaymentModel.create(payment);
+};
+
+export async function updatePaymentStatus(id: string, status: 'pending' | 'declined' | 'completed' |'cancelled'): Promise<Payment | null> {
+  const record = await PaymentModel.findOne({ _id: id });
+
+  if (record) {
+    record.status = status;
+    return await record.save();
+  }
+
+  return null;
+}
+
+export async function findPaymentById(id: string): Promise<Payment | null> {
+  return await PaymentModel.findOne({ _id: id });
+}
+
+export async function findPaymentsByUserId(userId: string, offset: number, limit: number): Promise<Payment[]> {
+  return await PaymentModel
+    .find({ userId })
+    .skip(offset)
+    .limit(limit)
+    .sort({ _id: -1 });
+}
+
+
