c544513ac067f03ded6e1f10e034fe74585e75ba
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-07-20 04:43:21 +0000

    chore(api-v4): development of the get-payments function

--- packages/api-v4/src/functions/payments.ts
+++ packages/api-v4/src/functions/payments.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { initializeDatabaseConfiguration } from "../config";
-import { findPaymentById } from "../models/payment";
+import { findPaymentById, findPaymentsByUserId } from "../models/payment";
 
 // GET: Get Paymentt by Id
 export async function getPaymentById(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
@@ -22,6 +22,71 @@ export async function getPaymentById(request: HttpRequest, context: InvocationCo
       status: 404,
       jsonBody: {
         error: "Payment record not found",
+      },
+    };
+  }
+};
+
+// GET: Get Payments
+export async function getPayments(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  context.log(`Http function getAllPayments processed request for url "${request.url}"`);
+
+  const offset = Number(request.query.get('offset')) || 0;
+  const limit = Number(request.query.get('limit')) || 10;
+  const { userId } = request.params;
+
+  if (!userId || userId === 'undefined') {
+    return {
+      status: 400,
+      jsonBody: {
+        error: 'UserId is missing',
+      },
+    };
+  }
+
+  if (offset < 0) {
+    return {
+      status: 400,
+      jsonBody: {
+        error: 'Offset must be greater than or equal to 0',
+      },
+    };
+  } else if (limit < 0) {
+    return {
+      status: 400,
+      jsonBody: {
+        error: 'Limit must be greater than or equal to 0',
+      },
+    };
+  } else if (offset > limit) {
+    return {
+      status: 400,
+      jsonBody: {
+        error: 'Offset must be less than or equal to limit',
+      },
+    };
+  }
+
+  try {
+    const payments = await findPaymentsByUserId(userId, offset, limit);
+
+    if (payments) {
+      return {
+        jsonBody: payments,
+      };
+    } else {
+      return {
+        status: 404,
+        jsonBody: {
+          error: 'Payments not found',
+        },
+      };
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      jsonBody: {
+        error: 'Internal Server Error',
       },
     };
   }
