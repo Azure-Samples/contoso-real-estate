191d411aa6b5f55edb27aac7d34714bb098d71f2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-07-20 03:27:02 +0000

    fix(api-v4): some important changes

--- packages/api-v4/src/functions/payments.ts
+++ packages/api-v4/src/functions/payments.ts
@@ -1,37 +1,27 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import { initializeDatabaseConfiguration } from '../config';
-import { findPaymentById } from '../models/payment';
+import { initializeDatabaseConfiguration } from "../config";
+import { findPaymentById } from "../models/payment";
 
-// GET: Get Payment by Id
+// GET: Get Paymentt by Id
 export async function getPaymentById(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log(`Http function getPaymentById processed request for url "${request.url}"`);
 
-  await initializeDatabaseConfiguration();
+  //await initializeDatabaseConfiguration();
 
   const id = request.params.id ?? '';
 
-  try {
-    const payment = await findPaymentById(id);
+  const payment = await findPaymentById(id);
 
   if (payment) {
     return {
       jsonBody: payment,
-    }
+    };
   } else {
     return {
       status: 404,
       jsonBody: {
-        error: 'Payment record not found',
-      },
-    };
-  }
-  } catch (error) {
-    return {
-      status: 500,
-      jsonBody: {
-        error: 'Internal Server Error',
+        error: "Payment record not found",
       },
     };
   }
 };
-
