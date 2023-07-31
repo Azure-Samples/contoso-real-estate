1f252a3d4b30d0cb145b961ed846b8681d480647
Natalia Venditto <nataliafrontend@gmail.com>
2023-07-31 16:41:36 +0200

    feat(portal): use Angular Signals (#230)
    * feat(portal): migrate to signals (#223)
    
    * chore: merging to test in code spaces
    
    * fix: call discount signals in templates
    
    ---------
    
    Co-authored-by: Sumit Parakh <sumit.thecoder@gmail.com>
    Co-authored-by: Wassim Chegham <github@wassim.dev>

--- packages/portal/src/app/checkoutpage/checkoutpage.component.ts
+++ packages/portal/src/app/checkoutpage/checkoutpage.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { ReservationService } from '../shared/reservation.service';
@@ -12,16 +12,16 @@ import { ReservationService } from '../shared/reservation.service';
   imports: [RouterModule, CommonModule, MatButtonModule],
 })
 export class CheckoutpageComponent implements OnInit {
-  result = 'error';
+  result = signal<CheckoutResult>('error');
 
   private route = inject(ActivatedRoute);
   private reservationService = inject(ReservationService);
 
   ngOnInit(): void {
-    this.result = this.route.snapshot.queryParams['result'];
+    this.result.set(this.route.snapshot.queryParams['result']);
 
     const reservationId = this.route.snapshot.queryParams['reservationId'];
-    if (reservationId && this.result === 'cancel') {
+    if (reservationId && this.result() === 'cancel') {
       this.reservationService.cancelReservation(reservationId);
     }
   }
--- packages/portal/src/app/searchpage/searchpage.component.ts
+++ packages/portal/src/app/searchpage/searchpage.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardListComponent } from '../shared/card-list/card-list.component';
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { SearchService } from '../shared/search/search.service';
 
 
@@ -20,11 +19,12 @@ import { SearchService } from '../shared/search/search.service';
 export class SearchpageComponent implements OnInit {
   searchForm!: FormGroup;
   isLoading = false;
-  listings: Listing[] = [];
   noresults = false;
   noresultsMessage = "The search returned 0 results. Please, try again...";
-  seatrchTermInvalid = false;
   searchTermInvalidMessage = "The search term must be at least 4 characters long. Please, try again...";
+
+  listings = signal<Listing[]>([]);
+  seatrchTermInvalid = signal(false);
 
   private searchService = inject(SearchService);
 
@@ -40,21 +40,21 @@ export class SearchpageComponent implements OnInit {
 
   onSubmit(): void {
     this.noresults = false;
-    this.seatrchTermInvalid = false;
+    this.seatrchTermInvalid.set(false);
     if (this.searchForm.value.term === "" || this.searchForm.invalid) {
-      this.seatrchTermInvalid = true;
+      this.seatrchTermInvalid.set(true);
       return;
     }
     this.searchService.getResults(this.searchForm.value.term)
       .subscribe((data) => {
         const results: SearchResult = data.data;
         const listingsResults: CleanResults = results.listings;
-        this.listings = listingsResults.data;
-        if (this.listings.length === 0 && this.searchForm.get('term')?.dirty) {
+        this.listings.set(listingsResults.data);
+        if (this.listings().length === 0 && this.searchForm.get('term')?.dirty) {
           this.noresults = true;
         }
         this.searchForm.reset();
-    });
+      });
   }
 
   ngOnDestroy(): void {
