00ca5050749e274db6ae1961b4f08f6291d2f5dd
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-07-31 21:27:04 +0000

    chore(api-v4): development of the 'get-reservation-by-id' function from v3 to v4

--- packages/api-v4/src/index.ts
+++ packages/api-v4/src/index.ts
@@ -8,13 +8,17 @@
 import { app } from "@azure/functions";
 import { httpTriggerTest } from './functions/httpTriggerTest';
 import { getUsers, getUserById } from './functions/users';
-import { getPaymentById, getPayments} from './functions/payments';
+import { getPaymentById, getPayments } from './functions/payments';
+import { getReservationById } from './functions/reservations';
 
+//#region Test Trigger Function
 app.get('httpTriggerTest', {
   authLevel: 'anonymous',
   handler: httpTriggerTest
 });
+//#endregion
 
+//#region User Function
 app.get('get-users', {
   route: 'users',
   authLevel: 'anonymous',
@@ -26,16 +30,28 @@ app.get('get-users-by-id', {
   authLevel: 'anonymous',
   handler: getUserById,
 });
+//#endregion
 
+//#region Payment Function
 app.get('get-payment-by-id', {
   route: 'payments/{id}',
   authLevel: 'anonymous',
   handler: getPaymentById
 });
 
+
 app.get('get-payments', {
   route: 'payments',
   authLevel: 'anonymous',
   handler: getPayments
 });
+//#endregion
+
+//#region Reservation Function
+app.get('get-reservation-by-id', {
+  route: 'reservations/{id}',
+  authLevel: 'anonymous',
+  handler: getReservationById
+});
+//#endregion
 
