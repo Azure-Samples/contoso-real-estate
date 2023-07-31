fec1eae678862f743b57c84d5e00ba1c01892a48
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-07-31 22:00:20 +0000

    chore(api-v4): development of the 'get-reservation' function from v3 to v4

--- packages/api-v4/src/functions/reservations.ts
+++ packages/api-v4/src/functions/reservations.ts
@@ -1,4 +1,4 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { findReservationById, findReservationsByUserId } from "../models/reservation";
 
 // GET: Get Reservation by Id
@@ -23,3 +23,70 @@ export async function getReservationById(request: HttpRequest, context: Invocati
   }
 };
 
+
+// GET: Get Reservations
+export async function getReservations(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  context.log(`Http function getReservations processed request for url "${request.url}"`);
+
+  const offset = Number(request.query.get('offset')) || 0;
+  const limit = Number(request.query.get('limit')) || 10;
+
+  const { userId } = request.params;
+
+  // UserID is the only required parameter
+  if (!userId || userId === 'undefined') {
+    return {
+      status: 400,
+      jsonBody: {
+        error: 'userId is missing',
+      },
+    };
+  }
+
+  if (offset < 0) {
+    return {
+      status: 400,
+      jsonBody: {
+        error: 'Offset must be greater than or equal to 0'
+      },
+    };
+  } else if (limit < 0) {
+    return {
+      status: 400,
+      jsonBody: {
+        error: 'Limit must be greater than or equal to 0'
+      },
+    };
+  } else if (offset > limit) {
+    return {
+      status: 400,
+      jsonBody: {
+        error: 'Offset must be less than or equal to limit'
+      },
+    };
+  }
+
+  try {
+    const reservations = await findReservationsByUserId(userId, offset, limit)
+
+    if (reservations) {
+      return {
+        jsonBody: reservations,
+      };
+    } else {
+      return {
+        status: 404,
+        jsonBody: {
+          error: "Reservation not found"
+        },
+      };
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      jsonBody: {
+        error: 'Internal Server Error'
+      },
+    };
+  }
+};
