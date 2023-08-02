a444a7fa74673f3073f983271229b16d8551fe87
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-08-02 02:29:44 +0000

    chore(api-v4): development of the 'post-favorite' function from v3 to v4

--- packages/api-v4/src/index.ts
+++ packages/api-v4/src/index.ts
@@ -10,7 +10,7 @@ import { httpTriggerTest } from './functions/httpTriggerTest';
 import { getUsers, getUserById, postUsers } from './functions/users';
 import { getPaymentById, getPayments, postPayment } from './functions/payments';
 import { getReservationById, getReservations } from './functions/reservations';
-import { getFavorites } from './functions/favorites';
+import { getFavorites, postFavorites } from './functions/favorites';
 import { getListingById, getListings } from './functions/listings';
 import { openApi } from "./functions/openapi";
 
@@ -80,6 +80,12 @@ app.get('get-favorites', {
   route: 'favorites',
   authLevel: 'anonymous',
   handler: getFavorites
+});
+
+app.post('post-favorites', {
+  route: 'favorites',
+  authLevel: 'anonymous',
+  handler: postFavorites
 });
 //#endregion
 
