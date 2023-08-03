5cbd4d65f6a0ec84f137f9e5f75289c5b524bd00
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-08-03 01:51:20 +0000

    refactor(api-v4): include try-catch block

--- packages/api-v4/src/functions/openapi.ts
+++ packages/api-v4/src/functions/openapi.ts
@@ -6,35 +6,36 @@ import * as swaggerUi from 'swagger-ui-dist'
 export async function openApi(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log(`Http function openApi processed request for url "${request.url}"`);
 
-  const filename = request.params.filename || 'index.html';
-  const format = request.params.format;
-  const swaggerUiDistPath = swaggerUi.getAbsoluteFSPath();
-  const filePath = swaggerUiDistPath + '/' + filename;
-  const swaggerDocument = yaml.load('openapi.yaml');
+  try {
+    const filename = request.params.filename || 'index.html';
+    const format = request.params.format;
+    const swaggerUiDistPath = swaggerUi.getAbsoluteFSPath();
+    const filePath = swaggerUiDistPath + '/' + filename;
+    const swaggerDocument = yaml.load('openapi.yaml');
 
-  let mimetype = 'text/html';
-  let fileContent = '';
+    let mimetype = 'text/html';
+    let fileContent = '';
 
-  if (format === 'json') {
-    return {
-      headers: {
-        "content-type": "application/json; charset=utf-8",
-      },
-      body: JSON.stringify(swaggerDocument),
+    if (format === 'json') {
+      return {
+        headers: {
+          "content-type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify(swaggerDocument),
+      }
     }
-  }
 
-  if (!existsSync(filePath)) {
-    return {
-      status: 404,
-      jsonBody: {
-        error: "File not found",
-      },
-    };
-  }
+    if (!existsSync(filePath)) {
+      return {
+        status: 404,
+        jsonBody: {
+          error: "File not found",
+        },
+      };
+    }
 
-  if (filename.endsWith('swagger-initializer.js')) {
-    fileContent = `
+    if (filename.endsWith('swagger-initializer.js')) {
+      fileContent = `
       window.onload = function() {
         window.ui = SwaggerUIBundle({
           spec: ${JSON.stringify(swaggerDocument)},
@@ -51,24 +52,34 @@ export async function openApi(request: HttpRequest, context: InvocationContext):
         });
       };
     `;
-  } else {
-    fileContent = readFileSync(filePath).toString('utf-8');
-  }
+    } else {
+      fileContent = readFileSync(filePath).toString('utf-8');
+    }
 
-  if (filename.endsWith('.css')) {
-    mimetype = 'text/css';
-  } else if (filename.endsWith('.js')) {
-    mimetype = 'application/javascript';
-  } else if (filename.endsWith('.png')) {
-    mimetype = 'image/png';
-  }
+    if (filename.endsWith('.css')) {
+      mimetype = 'text/css';
+    } else if (filename.endsWith('.js')) {
+      mimetype = 'application/javascript';
+    } else if (filename.endsWith('.png')) {
+      mimetype = 'image/png';
+    }
 
-  return {
-    headers: {
-      "content-type": `${mimetype}; charset=utf-8`,
-    },
-    body: fileContent,
-  };
+    return {
+      headers: {
+        "content-type": `${mimetype}; charset=utf-8`,
+      },
+      body: fileContent,
+    };
+  } catch (error: unknown) {
+    const err = error as Error;
+    context.error('Error...', err.message);
+    return {
+      status: 500,
+      jsonBody: {
+        error: 'Internal server error',
+      },
+    };
+  }
 };
 
 
