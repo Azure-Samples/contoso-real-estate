294823c745c7b9f6befa273dd5f968ec925f06a5
Patrick Murimi <kabugap02@gmail.com>
2023-08-23 03:22:29 +0000

    feat(portal): implement 404 page

--- packages/portal/src/app/shared/errors/not-found/not-found.component.ts
+++ packages/portal/src/app/shared/errors/not-found/not-found.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule, RouterModule],
   standalone: true,
 })
 export class NotFoundComponent {}
