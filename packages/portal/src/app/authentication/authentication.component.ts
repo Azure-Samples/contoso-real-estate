f28f7b66833d8bd09d4c15cb29dd7e7121cc5e67
Surya V <102609082+Surya-V-28@users.noreply.github.com>
2023-09-12 18:39:47 +0530

    fix(portal): update icons in login page #292 (#323)
    * style: added the logos on loginpage #292
    
    * style: add-logos-register-page-292
    
    * Update index.html on requested 
    
    Removed the material icons link sheet
    
    * Update authentication.component.scss
    
    Resolved the conflicts
    
    * Update package.json
    
    updatethe package version apollo angular
    
    ---------
    
    Co-authored-by: Wassim Chegham <github@wassim.dev>

--- packages/portal/src/app/authentication/authentication.component.ts
+++ packages/portal/src/app/authentication/authentication.component.ts
@@ -1,32 +1,40 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit, inject } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { DomSanitizer } from "@angular/platform-browser";
+import { MatIconRegistry, MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { ActivatedRoute, Router } from "@angular/router";
 import { AuthService } from "../shared/authentication/auth.service";
 import { TextBlockComponent } from "../shared/text-block/text-block.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faApple, faFacebook, faGithub, faGoogle, faMicrosoft, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: "app-authentication",
   templateUrl: "./authentication.component.html",
   styleUrls: ["./authentication.component.scss"],
-  imports: [CommonModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, TextBlockComponent],
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, TextBlockComponent, MatIconModule,FontAwesomeModule],
   standalone: true,
 })
 export class AuthenticationComponent implements OnInit {
+  public constructor(iconRegistry: MatIconRegistry, santizer: DomSanitizer) {
+    for (const provider of this.providers) {
+      iconRegistry.addSvgIcon(provider.id, santizer.bypassSecurityTrustResourceUrl(`../assets/company-logos/${provider.id}.svg`));
+    }
+  }
+
   redirectURL = "/home";
-
   providers = [
-    { name: "Microsoft", id: "microsoft" },
-    { name: "Facebook", id: "facebook" },
-    { name: "Google", id: "google" },
-    { name: "Twitter", id: "twitter" },
-    { name: "GitHub", id: "github" },
-    { name: "Apple", id: "apple" }
+    { name: "Microsoft", id: "microsoft", icon: faFacebook },
+    { name: "Facebook", id: "facebook", icon: faMicrosoft },
+    { name: "Google", id: "google", icon: faGoogle },
+    { name: "Twitter", id: "twitter", icon: faTwitter },
+    { name: "GitHub", id: "github", icon: faGithub },
+    { name: "Apple", id: "apple", icon: faApple }
   ];
-
   private router = inject(Router);
   private route = inject(ActivatedRoute);
   private authService = inject(AuthService);
