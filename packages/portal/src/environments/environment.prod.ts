f0c6d5a995de4d67bdd9ccd2509181892b2dc51f
Wassim Chegham <github@wassim.dev>
2023-11-02 22:01:01 +0100

    fix: use blog and graphql uris in prod
    Closes #392

--- packages/portal/src/environments/environment.prod.ts
+++ packages/portal/src/environments/environment.prod.ts
@@ -5,7 +5,5 @@ export const environment = {
   // If you hard-code the URL here, it will be used instead of the placeholder and
   // no replacement will be made.
   blogUrl: "{{SERVICE_BLOG_URI_PLACEHOLDER}}",
-  isCodespaces: process.env["CODESPACE_NAME"] ? true : false,
-  strapiGraphQlUriInCodespace: `https://${process.env["CODESPACE_NAME"]}-1337.${process.env["GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN"]}/graphql`,
-  strapiGraphQlUriFallback: "{{SERVICE_CMS_URI_PLACEHOLDER}}/graphql",
+  strapiGraphQlUri: "{{SERVICE_CMS_URI_PLACEHOLDER}}/graphql"
 };
