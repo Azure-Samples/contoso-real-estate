fcf3b3d61f0bf879b31cc2d385253d51b1f53dd7
Dina Berry (personal) <dfberry@users.noreply.github.com>
2023-11-10 17:51:18 +0000

    fix(infra): restore db from key vault secret

--- scripts/keyvault/read-secrets.sh
+++ scripts/keyvault/read-secrets.sh
@@ -8,18 +8,24 @@
 # load environment variables
 DIR="$(dirname "$0")"
 
-keyVaultName="$AZURE_KEY_VAULT_NAME"
-echo "[READ SECRETS] Key vault name: $keyVaultName"
+# get the filename of the bash script to create
+# to add key vault secrets into environment
+filename="$1"
+
+# Get Key vault name from environment or 2nd param of bash execution
+keyVaultName="${AZURE_KEY_VAULT_NAME:-$2}"
 
 # if key vault name is not set, exit
 if [[ -z "$keyVaultName" ]]; then
   echo "[READ SECRETS] Usage: ./read-secrets"
   echo "[READ SECRETS] Please set the AZURE_KEY_VAULT_NAME environment variable"
   exit 1
+else
+  echo "[READ SECRETS] Key vault name: $keyVaultName"
 fi
 
 # get all secrets from the key vault
-secrets=$(az keyvault secret list --vault-name "$AZURE_KEY_VAULT_NAME" --query "[].name" --output tsv)
+secrets=$(az keyvault secret list --vault-name "$keyVaultName" --query "[].name" --output tsv)
 echo "[READ SECRETS] $secrets"
 
 # if secrets are not set, exit
@@ -48,6 +54,6 @@ for secretId in $secrets; do
   secretNameSnakeCase=$(echo "$secretName" | sed -e 's/-/_/g')
 
   # export the secret as an environment variable
-  echo "export $secretNameSnakeCase=\"$secretValue\"" >> "$DIR/KeyVaultVariables.sh"
+  echo "export $secretNameSnakeCase=\"$secretValue\"" >> $filename
 done
 
