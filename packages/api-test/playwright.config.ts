b2c1fd38231e09c339221a9dc35bc326310ea669
Dina Berry <diberry@microsoft.com>
2023-12-01 22:13:06 +0000

    fix(test): change reporters

--- packages/api-test/playwright.config.ts
+++ packages/api-test/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list'],['html']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
