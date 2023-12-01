70ef1191719862a92f0f399330497764de45e9c5
Dina Berry <diberry@microsoft.com>
2023-12-01 20:52:16 +0000

    fix(test): api cleanup

--- packages/api-test/tests/function.api.spec.ts
+++ packages/api-test/tests/function.api.spec.ts
@@ -1,26 +1,28 @@
 import { test, expect  } from '@playwright/test';
 
 // create base URL from 3 sources:
-// 1. SERVICE_API_ENDPOINTS - set by GitHub Actions
-// 2. CODESPACE_NAME - set by GitHub Codespaces
-// 3. localhost:7071 - default
-const BASE_URL = process.env.SERVICE_API_ENDPOINTS && process.env.SERVICE_API_ENDPOINTS[0]
-  ? process.env.SERVICE_API_ENDPOINTS[0]
+// 1. Azure: JSON.parse(process.env.SERVICE_API_ENDPOINTS)[0] - output array as string from `./infra/main.bicep`
+// 2. GitHub Codespaces: `https://${process.env.CODESPACE_NAME}-${process.env.CODESPACE_PORT}.githubpreview.dev`
+// 3. Local development on a local machine: `localhost:7071`
+const BASE_URL = process.env.SERVICE_API_ENDPOINTS
+  ? JSON.parse(process.env.SERVICE_API_ENDPOINTS)[0]
   : process.env.CODESPACE_NAME
     ? `https://${process.env.CODESPACE_NAME}-${process.env.CODESPACE_PORT}.githubpreview.dev`
     : 'http://localhost:7072';
+
+console.log(`BASE_URL: ${BASE_URL}`);
 
 test.use({
   baseURL: BASE_URL
 });
 
 // Test that PostgreSQL DB is up and running
+// Default: listings returns array of items
 test('should get listings', async ({ request }) => {
 
   const urlsResponse = await request.get('/api/listings');
 
-  const responseOk = urlsResponse.ok();
-  expect(responseOk).toBeTruthy();
+  expect(urlsResponse.ok()).toBeTruthy();
 
   const responseJson = await urlsResponse.json();
   expect(Array.isArray(responseJson)).toBeTruthy();
@@ -28,12 +30,12 @@ test('should get listings', async ({ request }) => {
 });
 
 // Test that Cosmos DB is up and running
+// Default: users is empty so returns 404
 test('should get users', async ({ request }) => {
 
   const urlsResponse = await request.get('/api/users');
 
-  const responseOk = urlsResponse.ok();
-  expect(responseOk).toBeFalsy();
+  expect(urlsResponse.ok()).toBeFalsy();
 
   // Test for correct error status code
   // Assumes users table is empty
