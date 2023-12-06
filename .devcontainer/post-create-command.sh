a508873afca1aa8c075aca43c7dcd3dadf6a2d38
Dina Berry (personal) <dfberry@users.noreply.github.com>
2023-12-05 23:58:04 -0800

    fix(infra): add playwright deps to postCreateCommand script (#410)
    * fix(infra): add playwright deps to postCreateCommand script
    
    * chore: Update post-create-command.sh
    
    ---------
    
    Co-authored-by: Wassim Chegham <github@wassim.dev>

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -2,4 +2,10 @@
 
 sudo apt update
 npm i -g npm@latest fuzz-run
+
+# https://github.com/microsoft/playwright/issues/28331
+npx playwright install --with-deps
+
+# Install monorepo dependencies
 npm install
+
