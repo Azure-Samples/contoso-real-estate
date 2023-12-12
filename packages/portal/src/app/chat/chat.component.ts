fa8f3ac1a7fe68fdf2e34fc5d4922af62e5d7901
sinedied <noda@free.fr>
2023-12-12 09:55:46 +0100

    feat(portal): add guard for AI chat integration

---
+++ packages/portal/src/app/chat/chat.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.scss'
+})
+export class ChatComponent {
+
+}
