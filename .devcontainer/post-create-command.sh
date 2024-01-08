879efcd0766ebc536e7acabcc85c023af17e9c56
Dina Berry (MSFT) <diberry@microsoft.com>
2024-01-08 15:53:40 +0000

    fix(api): add azure function core tools to dev container startup

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -6,6 +6,9 @@ npm i -g npm@latest fuzz-run
 # https://github.com/microsoft/playwright/issues/28331
 npx playwright install --with-deps
 
+# Azure Functions core tools
+npm i -g azure-functions-core-tools@4 --unsafe-perm true
+
 # Install monorepo dependencies
 npm install
 
