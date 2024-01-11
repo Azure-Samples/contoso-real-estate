3bfe9c7147d5478a30ce84689cde90de22804644
Wassim Chegham <github@wassim.dev>
2024-01-11 17:08:19 +0100

    fix(strip): fix major deps issues (#416)
    * fix(strip): fix major deps issues
    
    Closes #386
    
    * chore: exclude legacy api from workspace

--- packages/stripe/src/plugins/stripe.ts
+++ packages/stripe/src/plugins/stripe.ts
@@ -9,7 +9,7 @@ export default fp(
     if (!config.secretKey || !config.publicKey || !config.webhookSecret) {
       fastify.decorate("stripe", undefined);
     } else {
-      fastify.decorate("stripe", new Stripe(config.secretKey, { apiVersion: "2022-11-15" }));
+      fastify.decorate("stripe", new Stripe(config.secretKey, { apiVersion: "2023-10-16" }));
     }
   },
   {
