e17ce09293436c5f1c0d46970a81c66b0d37b87e
Max Schmitt <max@schmitt.mx>
2024-02-15 14:16:30 +0000

    fix: post-create-command

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -3,9 +3,8 @@
 sudo apt update
 npm i -g npm@latest fuzz-run
 
-# https://github.com/microsoft/playwright/issues/28331
-npx playwright install --with-deps
-
 # Install monorepo dependencies
 npm install
 
+# https://github.com/microsoft/playwright/issues/28331
+npx playwright install --with-deps
