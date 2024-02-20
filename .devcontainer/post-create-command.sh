07c2b59e638a853188a5d7e2d9b266ca73f7884c
Natalia Venditto <nataliafrontend@gmail.com>
2024-02-20 11:11:53 +0100

    fix: fix interactive console for postintall

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -4,7 +4,7 @@ sudo apt update
 npm i -g npm@latest fuzz-run
 
 # https://github.com/microsoft/playwright/issues/28331
-npx playwright install --with-deps
+npx playwright install --with-deps -y
 
 # Install monorepo dependencies
 npm install
