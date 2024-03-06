5b86309f87bcd9e588b0680983fbb167fbdad32e
Natalia Venditto <nataliafrontend@gmail.com>
2024-03-06 10:11:41 +0100

    chore: change order of commands

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -4,7 +4,7 @@ sudo apt update
 npm i -g npm@latest fuzz-run
 
 # https://github.com/microsoft/playwright/issues/28331
-npx playwright install --with-deps -y
+npx -y playwright install --with-deps
 
 # Install monorepo dependencies
 npm install
