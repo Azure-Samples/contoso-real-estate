31ca7617ffd121f0ebd799900afcaeb7e8e93584
Diana Todea <33810708+didiViking@users.noreply.github.com>
2024-11-26 12:34:41 +0000

    build(portal): stating my own project

--- packages/portal/src/app/app.config.ts
+++ packages/portal/src/app/app.config.ts
@@ -1,3 +1,22 @@
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+  import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+  initializeFaro({
+    url: 'https://faro-collector-prod-eu-west-2.grafana.net/collect/244e157abc7d48c8433562ed71321327',
+    app: {
+      name: 'Frontend-marketing-app',
+      version: '1.0.0',
+      environment: 'production'
+    },
+    
+    instrumentations: [
+      // Mandatory, omits default instrumentations otherwise.
+      ...getWebInstrumentations(),
+
+      // Tracing package to get end-to-end visibility for HTTP requests.
+      new TracingInstrumentation(),
+    ],
+  });
 import { provideHttpClient } from "@angular/common/http";
 import { ApplicationConfig } from "@angular/core";
 import { provideAnimations } from "@angular/platform-browser/animations";
